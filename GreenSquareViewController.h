d3b3066dc32daf95246b55227bd81d6fa2331e57
Vitrum Mus <VitrumMus@gmail.com>
2013-06-01 16:56:01 -0400

    Feature-Setup: added a GLKViewController

---
+++ GreenSquareViewController.h
@@ -0,0 +1,14 @@
+//
+//  GreenSquareViewController.h
+//  Green Square
+//
+//  Created by Hamdan Javeed on 2013-06-01.
+//  Copyright (c) 2013 Glass Cube. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface GreenSquareViewController : GLKViewController
+
+@end
