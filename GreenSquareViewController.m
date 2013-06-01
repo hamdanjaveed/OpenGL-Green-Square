8ddaa7502580e146b805d1ec0b3a2f114ff71b64
Vitrum Mus <VitrumMus@gmail.com>
2013-06-01 17:10:07 -0400

    Feature-Green Square: barebone code added

--- GreenSquareViewController.m
+++ GreenSquareViewController.m
@@ -8,10 +8,46 @@
 
 #import "GreenSquareViewController.h"
 
-@interface GreenSquareViewController ()
-
+@interface GreenSquareViewController () <GLKViewDelegate>
+// an OpenGL ES context that is used for drawing
+@property (strong, nonatomic) EAGLContext *context;
 @end
 
 @implementation GreenSquareViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // initialize an OpenGL ES 2.0 context
+    self.context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
+    
+    if (!self.context) {
+        NSLog(@"Failed to create an ES context");
+    }
+    
+    // associate the context with our view
+    GLKView *view = (GLKView *)self.view;
+    view.context = self.context;
+    [EAGLContext setCurrentContext:self.context];
+}
+
+- (BOOL)shouldAutorotate {
+    return YES;
+}
+
+- (NSUInteger)supportedInterfaceOrientations {
+    return (UIInterfaceOrientationLandscapeLeft | UIInterfaceOrientationLandscapeRight);
+}
+
+# pragma mark - GLKViewDelegate
+
+- (void)glkView:(GLKView *)view drawInRect:(CGRect)rect {
+    glClearColor(0, 104.0/255.0, 55.0/255.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
+- (void)update {
+    
+}
 
 @end
