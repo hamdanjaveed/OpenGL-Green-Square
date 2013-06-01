644b9cc591458f59cfb4461eb99a04df59117560
Vitrum Mus <VitrumMus@gmail.com>
2013-06-01 16:39:53 -0400

    Initial Commit

---
+++ Green Square/GreenSquareAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GreenSquareAppDelegate.h
+//  Green Square
+//
+//  Created by Hamdan Javeed on 2013-06-01.
+//  Copyright (c) 2013 Glass Cube. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GreenSquareAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Green Square/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Green Square
+//
+//  Created by Hamdan Javeed on 2013-06-01.
+//  Copyright (c) 2013 Glass Cube. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GreenSquareAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GreenSquareAppDelegate class]));
+    }
+}
